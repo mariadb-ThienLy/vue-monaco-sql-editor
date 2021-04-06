e0a33d4fc11666fb79ad026a08b7ad5230297afe
Thien Ly <thien.ly@mariadb.com>
2021-04-06 09:00:58 +0300

    Prototyping sql editor with monaco editor

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+    <div id="app">
+        <h1 class="title">Editor view using codemirror</h1>
+        <!--   <editor-view :dist="dist" @on-change="onEditorChange" /> -->
+        <p>Query to sent via API</p>
+        <code>{{ queryData }}</code>
+    </div>
+</template>
+
+<script>
+/* import EditorView from './components/EditorVIew/index.vue' */
+
+export default {
+    name: 'App',
+    components: {
+        EditorView,
+    },
+    data() {
+        return {
+            dist: {
+                table1: ['col_A', 'col_B', 'col_C'],
+                table2: ['other_columns1', 'other_columns2'],
+            },
+            queryData: null,
+        }
+    },
+
+    methods: {
+        onEditorChange(v) {
+            this.queryData = v
+        },
+    },
+}
+</script>
+
+<style>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    margin: 60px;
+}
+.title {
+    text-align: center;
+}
+</style>
---
+++ src/components/EditorVIew/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <textarea ref="txt" class="txt" />
+</template>
+
+<script>
+export default {
+    name: 'editor-view',
+    props: {
+        dist: { type: Object, require: true },
+    },
+    data() {
+        return {
+            queryData: null,
+            editor: null,
+        }
+    },
+    watch: {
+        queryData: function (v) {
+            this.$emit('on-change', v)
+        },
+    },
+    mounted() {},
+}
+</script>
